ab1fa4299e5bde90a6c14443b3b313f33bea8038
p15197905 <p15197905@GWHXX583WIN014W>
2017-02-22 09:53:29 +0000

    Jacks cls and tstcls

---
+++ MyClassLibrary/clsCars.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary
+{
+    public class clsCar
+    {
+        public string CarColour { get; set; }
+        public string CarManufacturer { get; set; }
+        public string CarModel { get; set; }
+        public bool CarNeedsRepair { get; set; }
+        public int CarNumberOfDoors { get; set; }
+        public int CarNumberOfSeats { get; set; }
+        public string CarRegistrationPlate { get; set; }
+        public bool CarSold { get; set; }
+
+
+
+        public bool Find(string CarManufactuer)
+        {
+            return true;
+        }
+    }
+}

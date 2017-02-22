3574d8312487482855d109f4f5aa623cebb7b65a
p15197905 <p15197905@GWHXX583WIN014W>
2017-02-22 09:35:40 +0000

    Jacks Test Class

---
+++ TestFramework/tstCars.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestFramework
+{
+    [TestClass]
+    public class tstCars
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

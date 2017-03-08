d32e794fe402f57c2bfde39d63352825d80d2a4b
p15197905 <p15197905@GWHXX583WIN014W>
2017-03-08 10:44:28 +0000

    Comments and code

--- TestFramework/tstCars.cs
+++ TestFramework/tstCars.cs
@@ -10,89 +10,128 @@ namespace TestFramework
         [TestMethod]
         public void InstanceOk()
         {
+            //Create an instance of the class we want to create.
             clsCar AnCar = new clsCar();
+            //Test to see if it exists.
             Assert.IsNotNull(AnCar);
         }
 
         [TestMethod]
         public void CarManufacturerOK()
         {
+            //Create an instance of the class we want to create.
             clsCar AnCar = new clsCar();
+            //Create some test data assign to the property.
             string TestData = "VolksWagen";
+            //Assign the data to the property.
             AnCar.CarManufacturer = TestData;
+            //Test to see if the two values are equal.
             Assert.AreEqual(AnCar.CarManufacturer, TestData);
         }
 
         [TestMethod]
         public void CarModelOK()
         {
+            //Create an instance of the class we want to create.
             clsCar AnCar = new clsCar();
+            //Create some test data assign to the property.
             string TestData = "Golf";
+            //Assign the data to the property.
             AnCar.CarModel = TestData;
+            //Test to see if the two values are equal.
             Assert.AreEqual(AnCar.CarModel, TestData);
         }
 
         [TestMethod]
         public void CarColourOK()
         {
+            //Create an instance of the class we want to create.
             clsCar AnCar = new clsCar();
+            //Create some test data assign to the property.
             string TestData = "Blue";
+            //Assign the data to the property.
             AnCar.CarColour = TestData;
+            //Test to see if the two values are equal.
             Assert.AreEqual(AnCar.CarColour, TestData);
         }
 
         [TestMethod]
         public void CarRegistrationPlateOK()
         {
+            //Create an instance of the class we want to create.
             clsCar AnCar = new clsCar();
+            //Create some test data assign to the property.
             string TestData = "GV06 DTN";
+            //Assign the data to the property.
             AnCar.CarRegistrationPlate = TestData;
+            //Test to see if the two values are equal.
             Assert.AreEqual(AnCar.CarRegistrationPlate, TestData);
         }
 
         [TestMethod]
         public void CarNeedsRepairOK()
         {
+            //Create an instance of the class we want to create.
             clsCar AnCar = new clsCar();
+            //Create some test data assign to the property.
             Boolean TestData = true;
+            //Assign the data to the property.
             AnCar.CarNeedsRepair = TestData;
+            //Test to see if the two values are equal.
             Assert.AreEqual(AnCar.CarNeedsRepair, TestData);
         }
 
         [TestMethod]
         public void CarNumberOfDoorsOK()
         {
+            //Create an instance of the class we want to create.
             clsCar AnCar = new clsCar();
+            //Create some test data assign to the property.
             Int32 TestData = 5;
+            //Assign the data to the property.
             AnCar.CarNumberOfDoors = TestData;
+            //Test to see if the two values are equal.
             Assert.AreEqual(AnCar.CarNumberOfDoors, TestData);
         }
 
         [TestMethod]
         public void CarNumberOfSeatsOK()
         {
+            //Create an instance of the class we want to create.
             clsCar AnCar = new clsCar();
+            //Create some test data assign to the property.
             Int32 TestData = 7;
+            //Assign the data to the property.
             AnCar.CarNumberOfSeats = TestData;
+            //Test to see if the two values are equal.
             Assert.AreEqual(AnCar.CarNumberOfSeats, TestData);
         }
 
         [TestMethod]
         public void CarSoldOK()
         {
+            //Create an instance of the class we want to create.
             clsCar AnCar = new clsCar();
+            //Create some test data assign to the property.
             Boolean TestData = true;
+            //Assign the data to the property.
             AnCar.CarSold = TestData;
+            //Test to see if the two values are equal.
             Assert.AreEqual(AnCar.CarSold, TestData);
         }
 
         [TestMethod]
         public void FindMethodOK()
         {
+            //Create an instance of the class we want to create.
             clsCar AnCarID = new clsCar();
+            //Create a boolean variable to store the result of validation.
             Boolean Found = false;
+            //Create some test data to use with the method.
             Int32 CarID = 2;
+            //Invoke the method.
             Found = AnCarID.Find(CarID);
+            //Test to see that the result is correct.
             Assert.IsTrue(Found);
         }
 
@@ -132,7 +171,7 @@ namespace TestFramework
             //Invoke the method.
             Found = AnCarID.Find(CarID);
             //Check the Manufacturer.
-            if (AnCarID.CarModel != "DAVE")
+            if (AnCarID.CarModel != "i8")
             {
                 Ok = false;
             }

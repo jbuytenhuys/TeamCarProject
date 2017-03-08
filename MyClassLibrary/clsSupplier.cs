cb5b673eb1e3ac99b4def10da0edb8758320fd1a
p16220697 <p16220697@GWHXX583WIN015W>
2017-03-08 09:39:11 +0000

    clsSupplier class updated tstSupplier test methods updated

--- MyClassLibrary/clsSupplier.cs
+++ MyClassLibrary/clsSupplier.cs
@@ -4,71 +4,177 @@ namespace MyClassLibrary
 {
     public class clsSupplier
     {
+        private string mAddress;
+        private string mCity;
+        private string mCounty;
+        private DateTime mDateAdded;
+        private string mMoblie;
+        private string mName;
+        private string mPostcode;
+        private string mPostionInCompany;
+        private string mSupplierID;
+        private string mSupplierName;
+        private string mTitle;
+        private string mWorkExt;
+        private string mWorkNumber;
+
         public clsSupplier()
         {
         }
 
 
         public Boolean Active { get; set; }
-
-        private string mAddress;
         public string Address
         {
             get
-            {
-                return mAddress; // asks system to returmn data to view
-            }
-            set
-            {
-                mAddress = value;// asks system to edit data
-            }
-        }
-        public string City { get; set; }
-        public string County { get; set; }
-        public DateTime DateAdded { get; set; }
-        public long Mobile { get; set; }
-        public string Name { get; set; }
-        public string PostCode { get; set; }
-        public string PostionInCompany { get; set; }
-        public long SupplierID { get; set; }
+            //return the private data.
+            { return mAddress; }
 
-        private string mSupplierName;
+            set
+            //set the value pf the private data member.
+            { mAddress = value; }
+        }
+        public string City
+        {
+            get
+            //return the private data.
+            { return mCity; }
+
+            set
+            //set the value pf the private data member.
+            { mCity = value; }
+        }
+        public string County
+        {
+            get
+            //return the private data.
+            { return mCounty; }
+
+            set
+            //set the value pf the private data member.
+            { mCounty = value; }
+        }
+        public DateTime DateAdded
+        {
+            get
+            //return the private data.
+            { return mDateAdded; }
+
+            set
+            //set the value pf the private data member.
+            { mDateAdded = value; }
+        }
+
+        public string Mobile
+        {
+            get
+            //return the private data.
+            { return mMoblie; }
+
+            set
+            //set the value pf the private data member.
+            { mMoblie = value; }
+        }
+        public string Name
+        {
+            get
+            //return the private data.
+            { return mName; }
+
+            set
+            //set the value pf the private data member.
+            { mName = value; }
+        }
+        public string PostCode
+        {
+            get
+            //return the private data.
+            { return mPostcode; }
+
+            set
+            //set the value pf the private data member.
+            { mPostcode = value; }
+        }
+        public string PostionInCompany
+        {
+            get
+            //return the private data.
+            { return mPostionInCompany; }
+
+            set
+            //set the value pf the private data member.
+            { mPostionInCompany = value; }
+        }
+        public String SupplierID
+        {
+            get
+            //return the private data.
+            { return mSupplierID; }
+
+            set
+            //set the value pf the private data member.
+            { mSupplierID = value; }
+        }
         public string SupplierName
         {
             get
-            {
-                return mSupplierName;
+            //return the private data.
+            { return mSupplierName; }
 
-            }
             set
-            {
-                mSupplierName = value;
-            }
+            //set the value pf the private data member.
+            { mSupplierName = value; }
         }
-        public long WorkExt { get; set; }
 
-        private string mWorkNumber; //this creates a new varible
+        public string Title
+        {
+            get
+            //return the private data.
+            { return mTitle; }
+
+            set
+            //set the value pf the private data member.
+            { mTitle = value; }
+        }
+
+
+        public string WorkExt
+        {
+            get
+            //return the private data.
+            { return mWorkExt; }
+
+            set
+            //set the value pf the private data member.
+            { mWorkExt = value; }
+        }
+
         public string WorkNumber
         {
             get
-            {
-                return mWorkNumber; // asks system to returmn data to view
-            }
+            //return the private data.
+            { return mWorkNumber; }
+
             set
-            {
-                mWorkNumber = value;// asks system to edit data
-            }
+            //set the value pf the private data member.
+            { mWorkNumber = value; }
         }
 
-        public bool Find(Int64 SupplierID)
+        public bool Find(string supplierID)
         {
-            mWorkNumber = "01215478965";
-            return true;
-        }
-
-        public bool Find(string supplierName)
-        {
-            mSupplierName = "Gurdip";
+            mSupplierID = "1";
+            mAddress = "r";
+            mCity = "b";
+            mCounty = "w";
+            mMoblie = "078";
+            mName = "Gurdip";
+            mSupplierName = "BMW";
+            mPostcode = "B69";
+            mPostionInCompany = "HeadOfSales";
+            mTitle = "Mr";
+            mWorkExt = "313";
+            mWorkNumber = "0121";
+            mDateAdded = Convert.ToDateTime("08/03/2017");
             return true;
         }
     }

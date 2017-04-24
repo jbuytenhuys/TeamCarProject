aee1f862ca70e3a78670a8803e2b318e0392992d
p15185285 <p15185285@GWHXX583WIN015W>
2017-04-24 13:40:34 +0100

    Finished book, add update delete and filter all done

--- MyClassLibrary/clsCarRepairsCollection.cs
+++ MyClassLibrary/clsCarRepairsCollection.cs
@@ -12,34 +12,12 @@ namespace MyClassLibrary
         
         public clsCarRepairsCollection()
         {
-            //Variable for the index
-            Int32 Index = 0;
-            //Variable for the record count
-            Int32 RecordCount = 0;
             //object for data connection
-            clsDataConnection DataBase = new clsDataConnection();
+            clsDataConnection DB = new clsDataConnection();
             //Execute stored procedure
-            DataBase.Execute("sproc_tblCarRepairs_SelectAll");
-            //get the count of the records
-            RecordCount = DataBase.Count;
-            //while there are records to rpocess
-            while (Index < RecordCount)
-            {
-                //Create a blank CarRepair
-                clsCarRepairs Arepair = new clsCarRepairs();
-                //Read in the fields from the current record
-                Arepair.CarRepairID = Convert.ToInt32(DataBase.DataTable.Rows[Index]["CarRepairID"]);
-                Arepair.DaysInForRepair = Convert.ToInt32(DataBase.DataTable.Rows[Index]["DaysInForRepair"]);
-                Arepair.DeadlineDate = Convert.ToDateTime(DataBase.DataTable.Rows[Index]["DeadlineDate"]);
-                Arepair.PartPrice = Convert.ToDecimal(DataBase.DataTable.Rows[Index]["PartPrice"]);
-                Arepair.PartRequired = Convert.ToString(DataBase.DataTable.Rows[Index]["PartRequired"]);
-                Arepair.RepairStatus = Convert.ToBoolean(DataBase.DataTable.Rows[Index]["RepairStatus"]);
-                //Add the record to the private data member
-                mCarRepairList.Add(Arepair);
-                //point at the next record
-                Index++;
-            }
-
+            DB.Execute("sproc_tblCarRepairs_SelectAll");
+            //populate array list with database
+            PopulateArray(DB);
         }
 
         public List<clsCarRepairs> CarRepairList
@@ -106,6 +84,63 @@ namespace MyClassLibrary
             DB.AddParameter("@CarRepairID", mThisCarRepair.CarRepairID);
             //execute the stored procedure
             DB.Execute("sproc_tblCarRepairs_Archive");
+        }
+
+        //Updates existing record in the database
+        public void Update()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("@CarRepairID", mThisCarRepair.CarRepairID);
+            DB.AddParameter("@DaysInForRepair", mThisCarRepair.DaysInForRepair);
+            DB.AddParameter("@DeadlineDate", mThisCarRepair.DeadlineDate);
+            DB.AddParameter("@PartPrice", mThisCarRepair.PartPrice);
+            DB.AddParameter("@PartRequired", mThisCarRepair.PartRequired);
+            DB.AddParameter("@RepairStatus", mThisCarRepair.RepairStatus);
+            //execute stored procedure
+            DB.Execute("sproc_tblCarRepairs_Update");
+        }
+
+        public void FilterByPartRequired(string PartRequired)
+        {
+            //connect to database
+            clsDataConnection DB = new clsDataConnection();
+            //send the PartRequired parameter to the database
+            DB.AddParameter("@PartRequired", PartRequired);
+            //execute stored procedure
+            DB.Execute("sproc_tblCarRepairs_FilterByPartRequired");
+            //populate array list with the data table
+            PopulateArray(DB);
+        }
+
+        void PopulateArray(clsDataConnection DB)
+        {
+            //populates the array list based on the data table in the parameter DB
+            //var for the index
+            Int32 Index = 0;
+            //Variable for the record count
+            Int32 RecordCount;
+            //get the count of records
+            RecordCount = DB.Count;
+            //clear the private array list
+            mCarRepairList = new List<clsCarRepairs>();
+            //while there are records to rpocess
+            while (Index < RecordCount)
+            {
+                //Create a blank CarRepair
+                clsCarRepairs Arepair = new clsCarRepairs();
+                //Read in the fields from the current record
+                Arepair.CarRepairID = Convert.ToInt32(DB.DataTable.Rows[Index]["CarRepairID"]);
+                Arepair.DaysInForRepair = Convert.ToInt32(DB.DataTable.Rows[Index]["DaysInForRepair"]);
+                Arepair.DeadlineDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["DeadlineDate"]);
+                Arepair.PartPrice = Convert.ToDecimal(DB.DataTable.Rows[Index]["PartPrice"]);
+                Arepair.PartRequired = Convert.ToString(DB.DataTable.Rows[Index]["PartRequired"]);
+                Arepair.RepairStatus = Convert.ToBoolean(DB.DataTable.Rows[Index]["RepairStatus"]);
+                //Add the record to the private data member
+                mCarRepairList.Add(Arepair);
+                //point at the next record
+                Index++;
+            }
         }
     }
 }
--- TCPBackEnd/MechanicHomepage.aspx.cs
+++ TCPBackEnd/MechanicHomepage.aspx.cs
@@ -4,39 +4,56 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MyClassLibrary;
 
 public partial class MechanicHomepage : System.Web.UI.Page
 {
+    Int32 CarRepairID;
     protected void Page_Load(object sender, EventArgs e)
     {
         if (IsPostBack == false)
         {
             DisplayRepairs();
         }
-
+        CarRepairID = Convert.ToInt32(Session["CarRepairID"]);
         txtMechanicDeadlineDate.Text = DateTime.Today.AddDays(60).ToShortDateString();
+        
+        
     }
 
     void DisplayRepairs()
     {
-        MyClassLibrary.clsCarRepairsCollection Repairs = new MyClassLibrary.clsCarRepairsCollection();
+        clsCarRepairsCollection Repairs = new clsCarRepairsCollection();
         lstMechanicRepairs.DataSource = Repairs.CarRepairList;
         lstMechanicRepairs.DataValueField = "CarRepairID";
         lstMechanicRepairs.DataTextField = "PartRequired";
         lstMechanicRepairs.DataBind();
     }
 
+    void DisplayRecordData()
+    {
+        CarRepairID = Convert.ToInt32(Session["CarRepairID"]);
+        clsCarRepairsCollection CarRepair = new clsCarRepairsCollection();
+        CarRepair.ThisCarRepair.Find(CarRepairID);
+        txtMechanicRepairID.Text = CarRepair.ThisCarRepair.CarRepairID.ToString();
+        txtMechanicDeadlineDate.Text = CarRepair.ThisCarRepair.DeadlineDate.ToShortDateString();
+        txtMechanicPartRequired.Text = CarRepair.ThisCarRepair.PartRequired;
+        txtMechanicPartPrice.Text = CarRepair.ThisCarRepair.PartPrice.ToString();
+        txtMechanicDaysInForRepair.Text = CarRepair.ThisCarRepair.DaysInForRepair.ToString();
+        chkMechanicRepairStatus.Checked = CarRepair.ThisCarRepair.RepairStatus;
+    }
+
     void Add()
     {
         //create an instance
-        MyClassLibrary.clsCarRepairsCollection CarRepair = new MyClassLibrary.clsCarRepairsCollection();
+        clsCarRepairsCollection CarRepair = new clsCarRepairsCollection();
         //validate the data on webform
-        Boolean OK = CarRepair.ThisCarRepair.Valid(Convert.ToInt32(txtDaysInForRepair.Text), txtMechanicDeadlineDate.Text, Convert.ToDecimal(txtMechanicPartPrice.Text), txtMechanicPartRequired.Text);
+        Boolean OK = CarRepair.ThisCarRepair.Valid(Convert.ToInt32(txtMechanicDaysInForRepair.Text), txtMechanicDeadlineDate.Text, Convert.ToDecimal(txtMechanicPartPrice.Text), txtMechanicPartRequired.Text);
         //if data is okay then add to object
         if (OK == true)
         {
             //Fetch data entered by user
-            CarRepair.ThisCarRepair.DaysInForRepair = Convert.ToInt32(txtDaysInForRepair.Text);
+            CarRepair.ThisCarRepair.DaysInForRepair = Convert.ToInt32(txtMechanicDaysInForRepair.Text);
             CarRepair.ThisCarRepair.DeadlineDate = Convert.ToDateTime(txtMechanicDeadlineDate.Text);
             CarRepair.ThisCarRepair.PartPrice = Convert.ToDecimal(txtMechanicPartPrice.Text);
             CarRepair.ThisCarRepair.PartRequired = txtMechanicPartRequired.Text;
@@ -52,6 +69,47 @@ public partial class MechanicHomepage : System.Web.UI.Page
             lblMechanicError.Text = "There were problems with the data you have entered, please try again";
         }
     }
+    void Update()
+    {
+        //create an instance
+        clsCarRepairsCollection CarRepair = new clsCarRepairsCollection();
+        //validate the data on webform
+        Boolean OK = CarRepair.ThisCarRepair.Valid(Convert.ToInt32(txtMechanicDaysInForRepair.Text), txtMechanicDeadlineDate.Text, Convert.ToDecimal(txtMechanicPartPrice.Text), txtMechanicPartRequired.Text);
+        //if data is okay then add to object
+        if (OK == true)
+        {
+            //Find record to update
+            CarRepair.ThisCarRepair.Find(CarRepairID);
+            //Fetch data entered by user
+            CarRepair.ThisCarRepair.DaysInForRepair = Convert.ToInt32(txtMechanicDaysInForRepair.Text);
+            CarRepair.ThisCarRepair.DeadlineDate = Convert.ToDateTime(txtMechanicDeadlineDate.Text);
+            CarRepair.ThisCarRepair.PartPrice = Convert.ToDecimal(txtMechanicPartPrice.Text);
+            CarRepair.ThisCarRepair.PartRequired = txtMechanicPartRequired.Text;
+            CarRepair.ThisCarRepair.RepairStatus = chkMechanicRepairStatus.Checked;
+            //update record
+            CarRepair.Update();
+            Response.Redirect("MechanicHomepage.aspx");
+            lblMechanicError.Text = "Repair successfully added";
+        }
+        else
+        {
+            //report error
+            lblMechanicError.Text = "There were problems with the data you have entered, please try again";
+        }
+    }
+
+    void Filter()
+    {
+        //create an instance
+        clsCarRepairsCollection CarRepair = new clsCarRepairsCollection();
+        CarRepair.ThisCarRepair.PartRequired = txtMechanicFilter.Text;
+        CarRepair.FilterByPartRequired(CarRepair.ThisCarRepair.PartRequired);
+        lstMechanicRepairs.DataSource = CarRepair.CarRepairList;
+        lstMechanicRepairs.DataValueField = "CarRepairID";
+        lstMechanicRepairs.DataTextField = "PartRequired";
+        lstMechanicRepairs.DataBind();
+    }
+
 
 
     protected void btnMechanicLogout_Click(object sender, EventArgs e)
@@ -72,7 +130,7 @@ public partial class MechanicHomepage : System.Web.UI.Page
 
     protected void btnMechanicFilter_Click(object sender, EventArgs e)
     {
-
+        Filter();
     }
 
     protected void lstMechanicRepairs_SelectedIndexChanged(object sender, EventArgs e)
@@ -112,7 +170,7 @@ public partial class MechanicHomepage : System.Web.UI.Page
 
     protected void btnMechanicUpdateRepair_Click(object sender, EventArgs e)
     {
-
+        Update();
     }
 
     protected void btnMechanicDeleteRepair_Click(object sender, EventArgs e)
@@ -135,5 +193,27 @@ public partial class MechanicHomepage : System.Web.UI.Page
         }
     }
 
-    
+
+
+    protected void btnMechanicViewData_Click(object sender, EventArgs e)
+    {
+        Int32 CarRepairID;
+
+        if (lstMechanicRepairs.SelectedIndex != -1)
+        {
+            CarRepairID = Convert.ToInt32(lstMechanicRepairs.SelectedValue);
+            Session["CarRepairID"] = CarRepairID;
+            DisplayRecordData();
+
+        }
+        else
+        {
+            lblMechanicError.Text = ("Please select a record to view from the list");
+        }
+    }
+
+    protected void btnMechanicReset_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("MechanicHomepage.aspx");
+    }
 }

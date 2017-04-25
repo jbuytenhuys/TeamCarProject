88d159982acbb2fdeb68e92c759cc16e9a549a2b
p15197905 <p15197905@GWHXX582WIN007W>
2017-04-25 14:01:05 +0100

    finished filter and therefore book

--- TCPBackEnd/Secure/ReceptionistHomepage.aspx.cs
+++ TCPBackEnd/Secure/ReceptionistHomepage.aspx.cs
@@ -123,6 +123,10 @@ public partial class ReceptionistHomepage : System.Web.UI.Page
     {
         clsCarsCollection Cars = new clsCarsCollection();
         Cars.FilterByCarManufacturer(txtReceptionistFilterByCarManufacturer.Text);
+        lstBoxReceptionistListCars.DataSource = Cars.CarList;
+        lstBoxReceptionistListCars.DataValueField = "CarID";        
+        lstBoxReceptionistListCars.DataTextField = "CarRegistrationPlate";       
+        lstBoxReceptionistListCars.DataBind();
     }
 
     protected void btnReceptionistArchive_Click(object sender, EventArgs e)
@@ -204,15 +208,7 @@ public partial class ReceptionistHomepage : System.Web.UI.Page
     protected void btnReceptionistReset_Click(object sender, EventArgs e)
     {
         //reset the text boxes on the web form to original values.
-        txtReceptionistCarID.Text = "";
-        txtReceptionistCarManufacturer.Text = "Car Manufacturer";
-        txtReceptionistCarModel.Text = "Car Model";
-        txtReceptionistCarRegistrationPlate.Text = "Car Registration Plate";
-        txtReceptionistCarColour.Text = "Car Colour";
-        txtReceptionistCarNumberOfDoors.Text = "Number Of Doors";
-        txtReceptionistCarNumberOfSeats.Text = "Number Of Seats";
-        ChkBoxReceptionistNeedsRepair.Checked = false;
-        ChkBoxReceptionistSold.Checked = false;
+        Response.Redirect("ReceptionistHomepage.aspx");
     }
 
     protected void btnReceptionistFilterCars_Click(object sender, EventArgs e)
